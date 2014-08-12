4d719ae7d7a9c5b24ba8406f99514b19f10f24ca
Ian Harmon <ianmharmon@gmail.com>
2014-08-11 21:16:47 -0400

    Revert "Revert "Beginning making a GameState""
    This reverts commit ba28518f97d38c7976b10b02d312fc3e67a08283.

---
+++ server/Gamestate.js
@@ -0,0 +1,6 @@
+exports.Gamestate = Gamestate;
+function Gamestate(state) {
+	//This object is just a definition, no actual methods. this should be a generic state we can send a client at any time.
+	this.state = state;
+
+}
