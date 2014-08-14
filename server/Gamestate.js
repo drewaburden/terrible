7c7afcfc86b20021d7f2b50a392672d6660b0891
James Garner <jamesmatthewgarner@gmail.com>
2014-08-14 16:21:17 -0400

    gamestate fix

--- server/Gamestate.js
+++ server/Gamestate.js
@@ -1,5 +1,5 @@
 module.exports = Gamestate;
-function Gamestate(state) {
+function Gamestate(state, judge, players, responses) {
 	//This object is just a definition, no actual methods. this should be a generic state we can send a client at any time.
 	this.state = state;
 	this.judge = judge;
